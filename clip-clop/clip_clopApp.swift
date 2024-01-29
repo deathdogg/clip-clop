c9b9c2b9e0508f91e0068177eb2ac1cb1fd0b619
deathdogg <47575628+deathdogg@users.noreply.github.com>
2024-01-28 17:24:18 -0800

    Initial Commit

---
+++ clip-clop/clip_clopApp.swift
@@ -0,0 +1,17 @@
+//
+//  clip_clopApp.swift
+//  clip-clop
+//
+//  Created by Ricardo Herrera on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct clip_clopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
